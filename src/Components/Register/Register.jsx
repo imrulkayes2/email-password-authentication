4872c11c54c46d907b39d05846a6559ca034ad37
imrulkayes75 <ikbubt@gmail.com>
2024-01-14 00:06:01 +0600

    Email Verification And Update User Name To Firebase

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import auth from "../../firebase/firebase.init";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
@@ -9,10 +9,11 @@ const Register = () => {
     const [showPassword, setShowPassword] = useState(false);
     const handleSubmited = e => {
         e.preventDefault();
+        const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
         const check = e.target.terms.checked;
-        console.log(email, password, check);
+        console.log(name, email, password, check);
         setRegisterError('');
         setSuccess('');
         if (password.length < 6) {
@@ -30,18 +31,39 @@ const Register = () => {
         createUserWithEmailAndPassword(auth, email, password)
             .then(result => {
                 console.log(result.user);
-                setSuccess('You have register successfully');
+                if (result.user.emailVerified) {
+                    setSuccess('User Created successfully')
+                }
+                else {
+                    alert('please verify your email')
+                }
+                // update profile
+                updateProfile(result.user, {
+                    displayName: name,
+                    photoURL: "https://example.com/jane-q-user/profile.jpg"
+                })
+                    .then(console.log('profile updated'))
+                    .catch((error) => {
+                        console.log(error.message);
+                    })
+                sendEmailVerification(result.user)
+                    .then(() => {
+                        alert('please check your email for verification')
+                    })
             })
             .catch(error => {
                 console.log(error);
                 setRegisterError(error.message);
             })
+
+
     }
     return (
         <div className=" border">
             <div className="mx-auto md:w-1/2">
                 <h3 className=" text-3xl mb-5">Please Register</h3>
                 <form onSubmit={handleSubmited}>
+                    <input className="border w-3/4 mb-4" type="text" name="name" placeholder="Please enter your Name" id="" required /><br />
                     <input className="border w-3/4 mb-4" type="email" name="email" placeholder="Please enter your email" id="" required /><br />
                     <div className="relative">
                         <input className="border w-3/4"
